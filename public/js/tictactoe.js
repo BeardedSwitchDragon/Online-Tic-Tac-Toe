313e98039704a06ec48896a4ebe77a1b0863f0a8
Geddy Mukama <geddymaria.mukama@gmail.com>
2021-09-01 13:23:54 +1000

    Player can now click to place an X. Also refactored a bit of the HTML, concerning classes and ids etc.

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -1,1 +1,31 @@
 
+//Fetch document elements
+
+//Get all tictactoe squares
+
+const ticTacToeSquares = document.getElementsByClassName("tictactoe-button");
+
+//Game Logic
+
+//Places an X or O when called. squareID is the element ID of the button, and player is either "X" or "O"
+const playSquare = (squareID, player) => {
+    document.getElementById(squareID).textContent = player;
+};
+const checkWin = () => {
+
+};
+const updateGrid = () => {
+
+};
+
+//Iterates through the all the tictactoe grids (more specfically buttons). It then listens for clicks, and replaces the button text with X (placeholder).
+Array.from(ticTacToeSquares).forEach((square) => {
+    square.addEventListener("click", () => {
+        playSquare(square.id, "X");
+    });
+});
+
+
+// Client-side socket stuff
+
+
