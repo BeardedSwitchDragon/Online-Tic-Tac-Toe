8447742fa67036d4ba8c30a363e1eb085c603795
Geddy Mukama <geddymaria.mukama@gmail.com>
2021-09-03 11:52:11 +1000

    adjusted the port so it'll assign itself to env variable.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Server } = require("socket.io");
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 const io = new Server(server);
